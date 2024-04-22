6e4f6bce29eb36c09d08ef4282e804fa817a6739
Metehan Oz <metehanozdev@gmail.com>
2024-04-21 17:17:47 -0700

    minor fixes

--- components/features-blocks.tsx
+++ components/features-blocks.tsx
@@ -1,4 +1,3 @@
-
 export default function FeaturesBlocks() {
   return (
     <section className="relative">
@@ -29,7 +28,7 @@ export default function FeaturesBlocks() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="w-10 h-10"
+                className="w-10 h-10"
               >
                 <path d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 0 0-1.032-.211 50.89 50.89 0 0 0-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 0 0 2.433 3.984L7.28 21.53A.75.75 0 0 1 6 21v-4.03a48.527 48.527 0 0 1-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979Z" />
                 <path d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 0 0 1.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0 0 15.75 7.5Z" />
@@ -49,7 +48,7 @@ export default function FeaturesBlocks() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="w-10 h-10"
+                className="w-10 h-10"
               >
                 <path
                   fill-rule="evenodd"
@@ -72,7 +71,7 @@ export default function FeaturesBlocks() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="w-10 h-10"
+                className="w-10 h-10"
               >
                 <path
                   fill-rule="evenodd"
@@ -95,7 +94,7 @@ export default function FeaturesBlocks() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="w-10 h-10"
+                className="w-10 h-10"
               >
                 <path d="M5.625 3.75a2.625 2.625 0 1 0 0 5.25h12.75a2.625 2.625 0 0 0 0-5.25H5.625ZM3.75 11.25a.75.75 0 0 0 0 1.5h16.5a.75.75 0 0 0 0-1.5H3.75ZM3 15.75a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75ZM3.75 18.75a.75.75 0 0 0 0 1.5h16.5a.75.75 0 0 0 0-1.5H3.75Z" />
               </svg>
@@ -114,7 +113,7 @@ export default function FeaturesBlocks() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="w-10 h-10"
+                className="w-10 h-10"
               >
                 <path
                   fill-rule="evenodd"
@@ -137,7 +136,7 @@ export default function FeaturesBlocks() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="w-10 h-10"
+                className="w-10 h-10"
               >
                 <path
                   fill-rule="evenodd"
